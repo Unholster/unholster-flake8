2a6b31e8b2e90cae7031400ccedb9bcbc65d3638
Abdel Sandoval <abdel.sandoval@unholster.com>
2020-02-26 16:22:28 -0300

    add configuration

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='unholster-flake8',
+    version='0.0.1',
+    description='Flake8 Dependencies',
+    url='git@github.com:Unholster/unholster-flake8.git',
+    author='Unholster',
+    author_email='',
+    license='unlicense',
+    packages=['unholster-flake8'],
+    zip_safe=False
+)
